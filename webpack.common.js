3d04ff761c63adc08c2361437e10455f74590706
Serene Lim <enerestar@gmail.com>
2020-02-27 07:09:06 +0800

    Remove unused code
    Signed-off-by: Serene Lim <enerestar@gmail.com>

--- webpack.common.js
+++ webpack.common.js
@@ -26,4 +26,33 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         // publicPath: '/',
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader',
+                ],
+            },
+        ],
+    },
 };
