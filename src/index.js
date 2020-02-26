3c1240b11f1cdddeb5de650dcc0acce982077d63
Serene Lim <enerestar@gmail.com>
2020-02-26 22:00:13 +0800

    Add css and image to webpack config
    Signed-off-by: Serene Lim <enerestar@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './search.png';
 
 function component() {
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    const searchIcon = new Image();
+    searchIcon.src = Icon;
+
+    element.appendChild(searchIcon);
+    
     return element;    
 }
 
