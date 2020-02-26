b0729e21f00680e2d03cb580493677dfa3f0524f
Serene Lim <enerestar@gmail.com>
2020-02-26 23:46:44 +0800

    Add webpack watch and dev server for autoreloading
    Signed-off-by: Serene Lim <enerestar@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js',
         print: './src/javascript/print.js'
+    },
+    devServer: {
+        contentBase: './dist',
     },
     plugins: [
         new CleanWebpackPlugin(),
