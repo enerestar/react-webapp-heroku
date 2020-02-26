c188aac8c212570e942692e6de4efe9ad2362ba8
Serene Lim <enerestar@gmail.com>
2020-02-26 23:34:54 +0800

    Separate files into individual folders, update config path to accept multiple entries and its associated outputs
    Signed-off-by: Serene Lim <enerestar@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './search.png';
+import './css/style.css';
+import Icon from './images/search.png';
+import printMe from './javascript/print.js'
 
 function component() {
     const element = document.createElement('div');
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    const button = document.createElement('button');
+    button.innerHTML = 'Click me and check the console';
+    button.onclick=printMe;
 
     const searchIcon = new Image();
     searchIcon.src = Icon;
 
     element.appendChild(searchIcon);
-    
+    element.appendChild(button);
+
     return element;    
 }
 
