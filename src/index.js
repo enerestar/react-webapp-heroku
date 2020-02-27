f1c4b66fa0e2d5e4326126547192a0ace6796a3f
Serene Lim <enerestar@gmail.com>
2020-02-27 11:05:11 +0800

    Add @babel-preset-react to babelrc config, change index.js to use react components
    Signed-off-by: Serene Lim <enerestar@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,39 +3,47 @@ import './css/style.css';
 import Icon from './images/search.png';
 import printMe from './javascript/print.js'
 import { cube } from './javascript/math.js';
+import React from 'react';
+import ReactDowm from 'react-dom';
 
 console.log("helloooo " + process.env.NODE_ENV)
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode');
 }
 
-function component() {
-    // const element = document.createElement('div');
-    const element = document.createElement('pre');
-
-    element.innerHTML = [
-        'Hello webpack',
-        '5 cubed equals' + cube(5),
-    ].join('\n\n');
-
-    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    element.classList.add('title')
-
-    const button = document.createElement('button');
-    button.innerHTML = 'Search me ';
-    button.onclick=printMe;
-
-    const searchIcon = new Image();
-    searchIcon.src = Icon;
-
-    element.appendChild(searchIcon);
-    element.appendChild(button);
-
-    return element;    
+const Index = () => {
+    return <div>Hello React</div>
 }
 
-document.body.appendChild(component());
+ReactDOM.render(<Index/>, document.getElementById('index'));
+
+// function component() {
+//     // const element = document.createElement('div');
+//     const element = document.createElement('pre');
+
+//     element.innerHTML = [
+//         'Hello webpack',
+//         '5 cubed equals' + cube(5),
+//     ].join('\n\n');
+
+//     // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//     element.classList.add('hello');
+//     element.classList.add('title')
+
+//     const button = document.createElement('button');
+//     button.innerHTML = 'Search me ';
+//     button.onclick=printMe;
+
+//     const searchIcon = new Image();
+//     searchIcon.src = Icon;
+
+//     element.appendChild(searchIcon);
+//     element.appendChild(button);
+
+//     return element;    
+// }
+
+// document.body.appendChild(component());
 
 if (module.hot) {
     module.hot.accept('./javascript/print.js', function() {
