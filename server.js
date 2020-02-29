35876a651cca6b287125d20af9ae7fc0bdd6e55e
Serene Lim <enerestar@gmail.com>
2020-02-29 15:51:06 +0800

    Heroku's config
    Signed-off-by: Serene Lim <enerestar@gmail.com>

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = express();
-const config = require('./webpack.config.js');
+const config = require('./webpack.common.js');
 const compiler = webpack(config);
 
 app.use(webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath,
 }));
 
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000')
+app.listen(process.env.PORT || 5000, function() {
+    console.log('Example app listening on port 5000')
 })
